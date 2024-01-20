a2c0e2af67ca178835009c54f4a4bc36fe4d48e3
X <x@Xs-MacBook-Pro.local>
2024-01-20 19:30:23 +0400

    review section started

--- app.js
+++ app.js
@@ -32,3 +32,32 @@ var swiper = new Swiper(".product-row",{
       },
     },
   });
+  
+  var swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 1,
+      },
+    },
+  });
